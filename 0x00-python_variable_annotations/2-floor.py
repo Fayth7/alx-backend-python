c3d7499d2046eee5a5fbdd58feb0213b5e6d32d8
Fayth7 <flexflakes@gmail.com>
2023-12-07 10:23:21 +0000

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Basic annotations concat """
+
+import math
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of the given float.
+
+    Args:
+        n (float): The input float.
+
+    Returns:
+        int: The floor of the input float.
+    """
+    return math.floor(n)
+
+if __name__ == "__main__":
+    ans = floor(3.14)
+    print(ans == math.floor(3.14))
+    print(floor.__annotations__)
+    print("floor(3.14) returns {}, which is a {}".format(ans, type(ans)))
