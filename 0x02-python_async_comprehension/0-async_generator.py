571eb335b425929151ba36fa5fc0878bff54f010
Fayth7 <flexflakes@gmail.com>
2023-12-14 16:06:30 +0000

    task 0 corrections

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -12,10 +12,12 @@ import random
 from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
+    '''coroutine that takes no arguments.'''
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
+
 
 # Example usage
 async def print_yielded_values():
